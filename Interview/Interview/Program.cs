fd58afd89f1fca55ebcf8e22cb5cdef2dd4f03af
Rui Geng <rui.geng@navblue.aero>
2017-10-31 16:07:21 -0400

    QuestionOne

--- Interview/Interview/Program.cs
+++ Interview/Interview/Program.cs
@@ -10,6 +10,19 @@ namespace Interview
     {
         static void Main(string[] args)
         {
+            QuestionOne();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// If the fractional component of a is halfway between two integers, one of which is even
+        /// and the other odd, then the even number is returned. Note that this method returns a
+        /// Double instead of an integral type.
+        /// </summary>
+        public static void QuestionOne()
+        {
+            Console.WriteLine(Math.Round(6.5));
+            Console.WriteLine(Math.Round(11.5));
         }
     }
 }
