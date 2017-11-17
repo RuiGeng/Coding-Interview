c4c2d3cf28b6f3ef1e07482858c76f696d2d149b
Rui Geng <rui.geng@navblue.aero>
2017-11-17 17:23:18 -0500

    add a qustion

---
+++ Interview/Question9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Question9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "like for example $  you don't have $  network $  access";
+            Regex rgx = new Regex("\\$\\s+");
+            s = Regex.Replace(s, @"(\$\s+.*?)\$\s+", "$1$$");
+            Console.WriteLine("string is: {0}", s);
+        }
+    }
+}
