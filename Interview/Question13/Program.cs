0e8babc10884f51e12bfbf541c49c62feb0a5f59
Rui Geng <rui.geng@navblue.aero>
2017-11-22 10:54:49 -0500

    DI 1

---
+++ Interview/Question13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Question13
+{
+    /// <summary>
+    /// http://www.dotnettricks.com/learn/dependencyinjection/implementation-of-dependency-injection-pattern-in-csharp
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //1. Constructor Injection
+            Client client = new Client(new Service());
+            client.Start();
+        }
+
+        public interface IService
+        {
+            void Serve();
+        }
+
+        public class Service : IService
+        {
+            public void Serve()
+            {
+                Console.WriteLine("Service Called");
+            }
+        }
+
+        public class Client
+        {
+            private readonly IService service;
+
+            public Client(IService service)
+            {
+                this.service = service;
+            }
+
+            public void Start()
+            {
+                Console.WriteLine("Service Started");
+                service.Serve();
+            }
+        }
+    }
+}
