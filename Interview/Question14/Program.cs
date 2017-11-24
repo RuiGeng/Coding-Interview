5b0bd0d235ffe6c8b98428d5bfe93db5a36d3bd5
Rui Geng <rui.geng@navblue.aero>
2017-11-24 16:29:11 -0500

    new question

---
+++ Interview/Question14/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Question14
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(KmToMeters(Console.ReadLine()));
+            }
+        }
+
+        public static long? KmToMeters(string kilometers)
+        {
+            try
+            {
+                var k = Convert.ToDouble(kilometers);
+
+                if (k < 0)
+                {
+                    throw new Exception("invalid input");
+                }
+
+                return (long)(k * 1000);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return null;
+        }
+    }
+}
