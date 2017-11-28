b4d629577fec4152f90e4c1c18589a5c2b3e2201
Rui Geng <rui.geng@navblue.aero>
2017-11-28 18:08:15 -0500

    add new question

---
+++ Interview/Question15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Question15
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var b = Boxing(123);
+
+            var ub = Unboxing<int>(b);
+
+            Console.ReadLine();
+        }
+
+        public static object Boxing<T>(T t)
+        {
+            var o = Convert.ChangeType(t, typeof(object));
+            return o;
+        }
+
+        public static TType Unboxing<TType>(object o)
+        {
+            var returnValue = Convert.ChangeType(o, typeof(TType));
+            return (TType)returnValue;
+        }
+    }
+}
